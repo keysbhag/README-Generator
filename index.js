0fb7c6a98558dd14c0218560f9644b4855fe48a5
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-10-06 17:08:44 -0400

    Added default values

--- index.js
+++ index.js
@@ -15,25 +15,25 @@ const questions = [
         type: 'input',
         name: 'description',
         message: "Enter your description"
-        default: 'README.MD'
+        default: 'description here'
     },
     {
         type: 'input',
         name: 'installation',
         message: "Enter your installation"
-        default: 'README.MD'
+        default: 'installation method here'
     },
     {
         type: 'input',
         name: 'usage',
         message: "Enter your usage"
-        default: 'README.MD'
+        default: 'usage method here'
     },
     {
         type: 'input',
         name: 'credits',
         message: "Enter your credits"
-        default: 'README.MD'
+        default: 'any credits here'
     },
     {
         type: "list",
@@ -45,25 +45,23 @@ const questions = [
         type: 'input',
         name: 'contribute',
         message: "Enter your contributing list"
-        default: 'README.MD'
+        default: 'how to contribute here'
     },
     {
         type: 'input',
         name: 'tests',
         message: "Enter your Tests"
-        default: 'README.MD'
+        default: 'any tests here'
     },
     {
         type: 'input',
         name: 'username',
         message: "What is your username?"
-        default: 'README.MD'
     },
     {
         type: 'input',
         name: 'email',
         message: "What is your email?"
-        default: 'README.MD'
     }
 ];
 
